13e03ff98f465d6c8774a1787dc1258d9ea6b328
Jan Boháč <33024493+HonzaBoh@users.noreply.github.com>
2024-10-10 20:16:26 +0200

    Update wslFix.sh

--- wslFix.sh
+++ wslFix.sh
@@ -1,2 +1,3 @@
-NEWUSER=<your_username>
-useradd --create-home --shell /usr/bin/bash --user-group --groups  adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev,netdev --password $(read -sp Password: pw ; echo $pw | openssl passwd -1 -stdin) $NEWUSER
+#!/bin/bash
+USERNAME=$1
+useradd -m -G sudo,adm,netdev,dialout,plugdev $1
